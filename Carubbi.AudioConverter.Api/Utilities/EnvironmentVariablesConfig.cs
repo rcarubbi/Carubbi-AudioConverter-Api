06ae3de400f14894290210a2681bcb1fab00648d
Raphael Carubbi Neto <rcarubbi@gmail.com>
2020-06-16 18:26:37 +0100

    Add files via upload

---
+++ Carubbi.AudioConverter.Api/Utilities/EnvironmentVariablesConfig.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.IO;
+using System.Linq;
+
+namespace Carubbi.AudioConverter.Api.Utilities
+{
+    public class EnvironmentVariablesConfig
+    {
+        private bool _loaded;
+
+        public void CheckAddBinPath()
+        {
+            if (_loaded) return;
+
+            // find path to 'bin' folder
+            var binPath = AppDomain.CurrentDomain.BaseDirectory;
+            // get current search path from environment
+            var path = Environment.GetEnvironmentVariable("PATH") ?? "";
+
+            // add 'bin' folder to search path if not already present
+            if (path.Split(Path.PathSeparator).Any(part => part.Equals(binPath, StringComparison.CurrentCultureIgnoreCase)))
+                return;
+
+            path = string.Join(Path.PathSeparator.ToString(), new string[] { path, binPath });
+            Environment.SetEnvironmentVariable("PATH", path);
+            _loaded = true;
+        }
+    }
+}
